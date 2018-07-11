276e6e78187f45addce8f4ed973aed135001db51
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-11 17:39:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var currentUser = 'Grace Hopper';
 var welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const currentUser = 'edgar dijkstra';
-const shortGreeting = `Welcome, ${currentUser}`;
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+var currentUser = 'edgar dijkstra';
+var shortGreeting = `Welcome, ${currentUser}`;
 'edgar dijkstra'[0];
